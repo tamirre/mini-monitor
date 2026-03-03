c4c237cbecc416fd7424e8170a8736100c2a88a0
tamirre <tamir.d@web.de>
2026-03-03 21:14:38 +0100

    Trying out the C++ postgresql lib: libpqxx

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-gcc src/db_test.c -o db_test \
-	-I$(pg_config --includedir) \
-	-L$(pg_config --libdir) \
-	-lpq
+# gcc src/db_test.c -o db_test \
+# 	-I$(pg_config --includedir) \
+# 	-L$(pg_config --libdir) \
+# 	-lpq
 
+g++ -std=c++20 -Wall -Wextra -Wpedantic src/db_test.cpp -o db_test $(pkg-config --cflags --libs libpqxx)
 # cmake -S . -B build
 cmake --build build
 
